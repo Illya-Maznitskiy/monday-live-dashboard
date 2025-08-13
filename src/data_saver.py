fd72b9f435e1a0f33c17440ef357a38c11e5e9d5
Illia <illia.maznitskyi@gmail.com>
2025-08-13 17:33:50 +0200

    Update data_saver.py to save data in project root dir

--- src/data_saver.py
+++ src/data_saver.py
@@ -1,25 +1,30 @@
 import os
 import json
+from pathlib import Path
+
+
+PROJECT_ROOT = Path(__file__).parent.parent
 
 
 def save_json(data, filename="storage/tasks_data.json"):
     """Save data as formatted JSON file, creating the directory if needed."""
 
-    folder = os.path.dirname(filename)
+    # Always save in PROJECT_ROOT/storage
+    folder = PROJECT_ROOT / "storage"
     print(f"Folder: {folder}")
-    if not folder:
-        folder = "storage"
-        filename = os.path.join(folder, filename)
 
+    # Create storage folder if it doesn't exist
     os.makedirs(folder, exist_ok=True)
 
-    print(f"Saving data to {filename}")
+    # Combine folder + filename
+    file_path = folder / filename
+    print(f"Saving data to {file_path}")
 
     try:
-        with open(filename, "w", encoding="utf-8") as f:
+        with file_path.open("w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=2)
     except (IOError, json.JSONDecodeError) as e:
-        print(f"Error saving JSON to {filename}: {e}")
+        print(f"Error saving JSON to {file_path}: {e}")
         raise
     else:
         print("Saved data to the file")
